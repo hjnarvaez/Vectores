45ce0d84cda7b77b00be73df16e87975309707e7
Cristiano216 <rafaelobando2015@hotmail.com>
2016-11-21 10:03:02 -0500

    Update VectoresGitHub.java
    COMO MANDAR A IMPRIMIR EL RESULTADO ?? AYUDE INGE

--- vectoresGitHub/src/vectoresgithub/VectoresGitHub.java
+++ vectoresGitHub/src/vectoresgithub/VectoresGitHub.java
@@ -4,7 +4,39 @@ public class VectoresGitHub {
 
     public static void main(String[] args) {
         //2222
-        
+   
     }
-    
-}
+       
+    public static void main(String[] args) {
+        // TODO code application logic here
+        EJEMPLOcris objeto = new EJEMPLOcris();
+        objeto.vector();
+    }
+
+    public void vector() {      
+        int limite=10;
+        int i=1;
+        int s=2;
+        int A[];
+        int B[];
+        int result[];
+        A = new int [6];
+        B = new int [6];
+        
+        i=0;
+        do {
+        A[i] = +(i/(i+1));
+        B[s] = +(i/(i+1));
+        i++;
+                }while(i<=limite);
+        imprimir(A)(B);  
+     }  
+       public void imprimir(int[] A1) {
+        int i = 0;
+        while (i < A1.length) {
+            System.out.print(A1[i]);
+            i++;
+        }
+    }
+} 
+       
