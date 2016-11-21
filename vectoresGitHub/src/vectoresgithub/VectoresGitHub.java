bd3c106c422c6a998923b60468557464ee990137
Jorge Acosta <jlacosta@tecnologicoitca.edu.ec>
2016-11-21 13:25:33 -0500

    Update VectoresGitHub.java

--- vectoresGitHub/src/vectoresgithub/VectoresGitHub.java
+++ vectoresGitHub/src/vectoresgithub/VectoresGitHub.java
@@ -3,8 +3,6 @@ package vectoresgithub;
 public class VectoresGitHub {
 
     public static void main(String[] args) {
-        //2222
-        hola
+        System.out.print("Hola Mundo");
     }
-    
 }
