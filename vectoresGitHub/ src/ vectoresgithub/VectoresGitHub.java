2df95b91845356da7290d079e09fdaca1339088d
Jeff-amaya <jeff16-1995@hotmail.com>
2016-11-21 13:45:54 -0500

    Update and rename vectoresGitHub/src/vectoresgithub/VectoresGitHub.java to  vectoresGitHub/ src/ vectoresgithub/VectoresGitHub.java

--- vectoresGitHub/ src/ vectoresgithub/VectoresGitHub.java
+++ vectoresGitHub/ src/ vectoresgithub/VectoresGitHub.java
@@ -5,6 +5,10 @@ public class VectoresGitHub {
     public static void main(String[] args) {
         System.out.print("Hola Mundo");
         int A[];
+
+           public static void main(String[] args) {
+        System.out.print("Hola Mundo");
+        int A[];
         ste
     }
 }
