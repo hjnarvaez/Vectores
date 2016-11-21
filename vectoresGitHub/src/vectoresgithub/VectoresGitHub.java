256b95dfdffe866c05bb38a534d6ed34e5df8b67
edycala <pacala94@hotmail.com>
2016-11-21 10:07:26 -0500

    hai esta un poco

--- vectoresGitHub/src/vectoresgithub/VectoresGitHub.java
+++ vectoresGitHub/src/vectoresgithub/VectoresGitHub.java
@@ -8,3 +8,36 @@ public class VectoresGitHub {
     }
     
 }
+ Vectores obj=new Vectores();
+     obj.vectores();
+          
+}
+   private int vectores(){
+       int c;
+       int[] c
+   }     
+
+    
+
+    public class PruebaVector1 
+
+        ; {
+    private Scanner teclado;
+        private int[] imprimir;
+        private int[] vector;
+
+        public void cargar() {
+            teclado = new Scanner(System.in);
+            imprimir = new int[5];
+            for (int f = 0; f < 5; f++) {
+                System.out.print("Ingrese los numeros de los vectores");
+                vector[2] = teclado.nextInt();
+            }
+        }
+
+        public void imprimir() {
+            for (int f = 0; f < 5; f++) {
+                System.out.println([crear]
+            
+        
+    
